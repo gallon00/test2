bd7d0fce5d1201d91adf41691416bcaa5c89b72c
miaoziyang <miaoziyang0128@163.com>
2020-08-13 08:51:26 +0800

    添加了 类中 Get 和Set 方法

--- src/main/java/com/czxy/domain/Student.java
+++ src/main/java/com/czxy/domain/Student.java
@@ -9,4 +9,30 @@ import lombok.Data;
 public class Student {
     private String id;
     private String sname;
+
+    private  String age ;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/czxy/pojo/Person.java
+++ src/main/java/com/czxy/pojo/Person.java
@@ -7,4 +7,38 @@ public class Person {
     private  String sex;
 
     private  Integer age;
+
+    private  String info;
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
